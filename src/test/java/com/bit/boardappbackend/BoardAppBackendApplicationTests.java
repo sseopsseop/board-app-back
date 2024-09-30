46d57298663681e2b6aefb1630285f6ee4e6509e
sseopsseop <ponde119@outlook.com>
2024-09-30 10:27:40 +0900

    test code 주석

--- src/test/java/com/bit/boardappbackend/BoardAppBackendApplicationTests.java
+++ src/test/java/com/bit/boardappbackend/BoardAppBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.bit.boardappbackend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BoardAppBackendApplicationTests {
 
     @Test
